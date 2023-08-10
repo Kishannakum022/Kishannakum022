f53b4ce07b9018407625364bf98987a56e349306
Kishannakum022 <141633590+Kishannakum022@users.noreply.github.com>
2023-08-10 14:32:55 +0530

    Add files via upload

---
+++ KISHANNA.C
@@ -0,0 +1,13 @@
+/*
+def:print hello world
+dev:nakum kishan B.
+date:3-8-2023
+*/
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+	clrscr();//clecr screen
+	printf("hellow world");
+	getch();
+}
